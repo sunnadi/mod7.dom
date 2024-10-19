21ae1053593baaf6d9e12e0a0a7bbb1e7b924f6e
Suneliya <149096669+sunnadi@users.noreply.github.com>
2024-10-19 23:07:30 +0500

    Add files via upload

---
+++ IObserver.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+public interface IObserver
+{
+    void Update(string currency, double rate);
+}
+public interface ISubject
+{
+    void Attach(IObserver observer);
+    void Detach(IObserver observer);
+    void Notify();
+}
+
+public class CurrencyExchange : ISubject
+{
+    private readonly List<IObserver> _observers = new List<IObserver>();
+    private double _usdRate;
+
+    public double UsdRate
+    {
+        get => _usdRate;
+        set
+        {
+            _usdRate = value;
+            Notify(); 
+        }
+    }
+
+    public void Attach(IObserver observer)
+    {
+        _observers.Add(observer);
+    }
+
+    public void Detach(IObserver observer)
+    {
+        _observers.Remove(observer);
+    }
+
+    public void Notify()
+    {
+        foreach (var observer in _observers)
+        {
+            observer.Update("USD", _usdRate);
+        }
+    }
+}
+public class User : IObserver
+{
+    public string Name { get; }
+
+    public User(string name)
+    {
+        Name = name;
+    }
+
+    public void Update(string currency, double rate)
+    {
+        Console.WriteLine($"Пользователь {Name} получил обновление: Курс {currency} - {rate}");
+    }
+}
+public class Application : IObserver
+{
+    public void Update(string currency, double rate)
+    {
+        Console.WriteLine($"Приложение получило обновление: Курс {currency} - {rate}");
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var currencyExchange = new CurrencyExchange();
+
+        var user1 = new User("Лидия");
+        var user2 = new User("Мария");
+        var application = new Application();
+
+        currencyExchange.Attach(user1);
+        currencyExchange.Attach(user2);
+        currencyExchange.Attach(application);
+
+        Console.WriteLine("Изменение курса USD:");
+        currencyExchange.UsdRate = 75.5;
+
+        currencyExchange.Detach(user1);
+        Console.WriteLine("Изменение курса USD:");
+        currencyExchange.UsdRate = 76.0;
+    }
+}
---
+++ Strategy.cs
@@ -0,0 +1,74 @@
+﻿using System;
+public interface IPaymentStrategy
+{
+    void Pay(double amount);
+}
+
+public class CreditCardPayment : IPaymentStrategy
+{
+    public void Pay(double amount)
+    {
+        Console.WriteLine($"Оплата {amount} с использованием банковской карты.");
+    }
+}
+public class PayPalPayment : IPaymentStrategy
+{
+    public void Pay(double amount)
+    {
+        Console.WriteLine($"Оплата {amount} через PayPal.");
+    }
+}
+
+public class CryptocurrencyPayment : IPaymentStrategy
+{
+    public void Pay(double amount)
+    {
+        Console.WriteLine($"Оплата {amount} с использованием криптовалюты.");
+    }
+}
+
+public class PaymentContext
+{
+    private IPaymentStrategy _paymentStrategy;
+
+    public void SetPaymentStrategy(IPaymentStrategy paymentStrategy)
+    {
+        _paymentStrategy = paymentStrategy;
+    }
+
+    public void ExecutePayment(double amount)
+    {
+        _paymentStrategy.Pay(amount);
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var paymentContext = new PaymentContext();
+
+        Console.WriteLine("Выберите способ оплаты: 1 - Карта, 2 - PayPal, 3 - Криптовалюта");
+        string choice = Console.ReadLine();
+
+        switch (choice)
+        {
+            case "1":
+                paymentContext.SetPaymentStrategy(new CreditCardPayment());
+                break;
+            case "2":
+                paymentContext.SetPaymentStrategy(new PayPalPayment());
+                break;
+            case "3":
+                paymentContext.SetPaymentStrategy(new CryptocurrencyPayment());
+                break;
+            default:
+                Console.WriteLine("Некорректный выбор.");
+                return;
+        }
+
+        Console.WriteLine("Введите сумму для оплаты:");
+        double amount = Convert.ToDouble(Console.ReadLine());
+        paymentContext.ExecutePayment(amount);
+    }
+}
